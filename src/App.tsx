acc50cbd781887ac16ec90edfcd421220006ac91
Alejandro Dev. <aeinformatico@gmail.com>
2024-01-15 14:15:05 +0000

    Add basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+type PostProps = {
+  post: {
+    id: string;
+    text: string;
+  };
+};
+
+const Post = ({ post }: PostProps) => (
+  <div key={post.id} className="bg-white rounded-md mt-4">
+    <p className="text-dark-body px-2 py-1 text-sm m-0">{post.text}</p>
+  </div>
+);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container sm mx-auto">
+        <div className="flex flex-col h-screen">
+          <header className="h-16 bg-header p-4">
+            <h2 className="text-dark-body text-lg">Posts</h2>
+          </header>
+          <main className="flex-grow overflow-auto flex-col-reverse bg-body">
+            <div className="px-4 py-4">
+              <div>
+                <Post post={{ id: "1", text: "Hello" }} />
+              </div>
+              <div>
+                <Post post={{ id: "2", text: "World" }} />
+              </div>
+            </div>
+          </main>
+          <footer className="h-40 bg-header">
+            <div className="px-2 py-4 flex flex-col gap-2">
+              <div>
+                <button
+                  className="bg-white text-dark-body text-sm px-4 py-2 border-light-body border-2 border-solid rounded-xl"
+                  onClick={() => {
+                    console.log("Clicked");
+                  }}
+                >
+                  Click me
+                </button>
+              </div>
+            </div>
+          </footer>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
