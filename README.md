ada6062b429bb65071a14eed40a0cc47d9382254
Alejandro Dev. <aeinformatico@gmail.com>
2024-01-16 09:43:26 +0000

    Add initial challenge

--- README.md
+++ README.md
@@ -1,30 +1,56 @@
-# React + TypeScript + Vite
+# Project
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Simple application that lists posts from [https://graphqlzero.almansi.me/](https://graphqlzero.almansi.me/).
 
-Currently, two official plugins are available:
+_All operations of this app are only the ones supported by this API_.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+# Context
 
-## Expanding the ESLint configuration
+You are now in charge of the project and there is no other developer. All the documentation there is for this project is this file.
+Unfortunately, you can't contact the previous team as they are in a sabbatical, though you have full ownwership and can make any change you want. You are in charge now.
+Feel free to use any library you want, make any change in the code, delete, create... It is _your_ project now.
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
+# Tasks
 
-- Configure the top-level `parserOptions` property like this:
+Product Owner has requested some tasks, and after some technical grooming they map to the following:
 
-```js
-export default {
-  // other rules...
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
+- [ ] Display all posts in by `id` `DESC`.
+- [ ] Only list 10 items in total
+- [ ] Implement the logic for the different buttons. New elements use `<NewPostItem />`. New posts are shown on top of the list.
+- [ ] Write the necessary tests to ensure new changes don't break the current logic, and tests for the new logic you add.
+
+From the technical side we want to:
+
+- [ ] Fix warnings in the code, linter and types
+- [ ] Write a technical description of the approach taken to implement the changes, why this approach has been taken, and how it can be further improved to allow more changes in a scalable way.
+- [ ] Write a technical and detailed description on what is in your opinion the best strategy to allow the app to run in environments with shaky internet connection (slow connection, intermittent connection, no connection...).
+
+_NOTE_: In case you run out of time or your solution becomes too ambitious, just write in plain English about your solution and how you would make it happen.
+
+# Deliverables:
+
+- Link to a Git repository (set to public or invite @ainformatico)
+
+# Development
+
+We are using:
+
+- Server: [Vite](https://vitejs.dev/)
+- GraphQL Client: [Apollo Client](https://www.apollographql.com/docs/react/)
+- Styles: [TailwindCSS](https://tailwindcss.com/docs/installation)
+
+To run your development environment:
+
+```bash
+yarn dev
 ```
 
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+To run the linter:
+
+```bash
+yarn lint
+```
+
+```bash
+yarn compile
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,121 @@
 import "./App.css";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  useQuery,
+} from "@apollo/client";
 
-type PostProps = {
-  post: {
-    id: string;
-    text: string;
-  };
+import { Post } from "./__generated__/graphql";
+
+import { gql } from "@apollo/client";
+
+const GET_POSTS = gql`
+  query Posts {
+    posts {
+      data {
+        id
+        title
+        body
+        user {
+          id
+          name
+          email
+        }
+      }
+    }
+  }
+`;
+
+const client = new ApolloClient({
+  uri: "https://graphqlzero.almansi.me/api",
+  cache: new InMemoryCache(),
+});
+
+type Post = {
+  id: number;
+  title: string;
+  body: string;
 };
 
-const Post = ({ post }: PostProps) => (
-  <div key={post.id} className="bg-white rounded-md mt-4">
-    <p className="text-dark-body px-2 py-1 text-sm m-0">{post.text}</p>
+const PostItem = (post: Post) => (
+  <div className="bg-white rounded-md px-2 py-1 mt-4">
+    <h2>{post.title}</h2>
+    <p className="text-dark-body text-sm m-0">{post.body}</p>
   </div>
 );
+
+const NewPostItem = (post: Post) => (
+  <div key={post.id} className="bg-secondary rounded-md px-2 py-1 mt-4">
+    <h2>{post.title}</h2>
+    <p className="text-dark-body text-sm m-0">{post.body}</p>
+  </div>
+);
+
+const Main = () => {
+  const { loading, error, data } = useQuery(GET_POSTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return (
+    <div className="flex flex-col h-screen">
+      <header className="h-16 bg-header p-4">
+        <h2 className="text-dark-body text-lg">Posts</h2>
+      </header>
+      <main className="flex-grow overflow-auto flex-col-reverse bg-body">
+        <div className="px-4 py-4">
+          {data.posts.data.map((post: PostProps) => (
+            <PostItem {...post} />
+          ))}
+        </div>
+      </main>
+      <footer className="h-40 bg-header">
+        <div className="px-2 py-4 flex flex-col gap-2">
+          <div>
+            <button
+              className="bg-white text-dark-body text-sm px-4 py-2 border-light-body border-2 border-solid rounded-xl"
+              onClick={() => {
+                console.log("Create");
+              }}
+            >
+              Create a new post
+            </button>
+          </div>
+          <div>
+            <button
+              className="bg-white text-dark-body text-sm px-4 py-2 border-light-body border-2 border-solid rounded-xl"
+              onClick={() => {
+                console.log("Update");
+              }}
+            >
+              Update post
+            </button>
+          </div>
+          <div>
+            <button
+              className="bg-white text-dark-body text-sm px-4 py-2 border-light-body border-2 border-solid rounded-xl"
+              onClick={() => {
+                console.log("Delete");
+              }}
+            >
+              Delete post
+            </button>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
 
 function App() {
   return (
     <>
-      <div className="container sm mx-auto">
-        <div className="flex flex-col h-screen">
-          <header className="h-16 bg-header p-4">
-            <h2 className="text-dark-body text-lg">Posts</h2>
-          </header>
-          <main className="flex-grow overflow-auto flex-col-reverse bg-body">
-            <div className="px-4 py-4">
-              <div>
-                <Post post={{ id: "1", text: "Hello" }} />
-              </div>
-              <div>
-                <Post post={{ id: "2", text: "World" }} />
-              </div>
-            </div>
-          </main>
-          <footer className="h-40 bg-header">
-            <div className="px-2 py-4 flex flex-col gap-2">
-              <div>
-                <button
-                  className="bg-white text-dark-body text-sm px-4 py-2 border-light-body border-2 border-solid rounded-xl"
-                  onClick={() => {
-                    console.log("Clicked");
-                  }}
-                >
-                  Click me
-                </button>
-              </div>
-            </div>
-          </footer>
+      <ApolloProvider client={client}>
+        <div className="container sm mx-auto">
+          <Main />
         </div>
-      </div>
+      </ApolloProvider>
     </>
   );
 }
